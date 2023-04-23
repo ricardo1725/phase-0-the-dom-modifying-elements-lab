1e48d21d3ee318334a28b41fef17028ca9794894
Ricardo Nava <ricardo1725@gmail.com>
2023-04-23 19:23:10 -0400

    DOMinator

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  const titleCased = () => {
+    return tutorials.map(function(tutorial) {
+      return tutorial.split(' ').map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
+    });
+  }
+  
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "YOUR-NAME is the champion";
+  
+  document.body.removeChild(document.querySelector("main#main"));
+  
+  document.body.appendChild(newHeader);
+  
